e6148d333a1afcd9c10579e591733223dc03a6f3
Brian <brian.kawa@student.moringaschool.com>
2023-10-02 11:54:10 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+let employee = {name: "John Mark", streetAddress: "851 Avenue Lane"};
+console.log(employee);
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+let updated = {...employee, [key]: value};
+return updated;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+employee[key] = value;
+
+return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+let newEmployee = {...employee};
+delete newEmployee[key];
+return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+delete employee[key];
+return employee; 
+}
